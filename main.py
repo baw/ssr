4043a9d2b77cd190d5336417d13d3f00e722de54
Brian Weiser <brian.weiser@adroll.com>
2018-07-17 00:29:35 -0700

    add python react

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
+import os
 
 from flask import Flask, render_template, request, jsonify
+from react.render import render_component
 app = Flask(__name__)
 
 app.static_folder=app.root_path + '/build/static/'
@@ -13,10 +15,14 @@ def index(path):
     props = {
         'items': items
     }
+    rendered = render_component(
+        os.path.join(os.getcwd(), 'src', 'ServerIndex.js'),
+        props
+    )
 
     asset_manifest_json_str = open('./build/asset-manifest.json').read()
     asset_manifest = json.loads(asset_manifest_json_str)
-    return render_template('index.html', asset_manifest=asset_manifest, props=json.dumps(props))
+    return render_template('index.html', asset_manifest=asset_manifest, rendered=rendered, props=json.dumps(props))
 
 
 @app.route('/api/add_item', methods=['POST'])
